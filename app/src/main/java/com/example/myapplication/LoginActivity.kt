0b5705302d3c807840517c73354457a8c468024f
EslamShaban74 <eslamshaban12328@gmail.com>
2023-01-09 19:04:47 +0200

    firebase

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -7,10 +7,12 @@ import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() ,TextWatcher{
+class LoginActivity : AppCompatActivity(), TextWatcher {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -22,32 +24,68 @@ class LoginActivity : AppCompatActivity() ,TextWatcher{
 
 
 
-        loginBtn.setOnClickListener{
+        loginBtn.setOnClickListener {
 
-            progressBar.visibility = View.VISIBLE
-            val handler = Handler()
-            handler.postDelayed(Runnable {
-                progressBar.visibility = View.GONE
-                val sharedPreferences = getSharedPreferences("Login", Context.MODE_PRIVATE)
-                val sharedPrefs = sharedPreferences.edit()
-                sharedPrefs.putString("userName",email.text.toString())
-                sharedPrefs.putString("password",password.text.toString())
-                sharedPrefs.apply()
-                val intent = Intent(this,LogoutActivity::class.java)
-                startActivity(intent)
-            }, 3000)
+            if (email.text.isNotEmpty() && password.text.isNotEmpty()) {
+                val auth = FirebaseAuth.getInstance()
+                auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+                    }.addOnFailureListener {
+                        println(it.message.toString())
+                        Toast.makeText(this, it.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+            } else {
+                Toast.makeText(this, "Email and password can not be empty", Toast.LENGTH_LONG)
+                    .show()
+
+            }
 
         }
+        signUp.setOnClickListener {
+
+            if (email.text.isNotEmpty() && password.text.isNotEmpty()) {
+                val auth = FirebaseAuth.getInstance()
+                auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+                    }.addOnFailureListener {
+                        println(it.message.toString())
+                        Toast.makeText(this, it.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+            } else {
+                Toast.makeText(this, "Email and password can not be empty", Toast.LENGTH_LONG)
+                    .show()
+
+            }
+        }
+
+
+//            progressBar.visibility = View.VISIBLE
+//            val handler = Handler()
+//            handler.postDelayed(Runnable {
+//                progressBar.visibility = View.GONE
+//                val sharedPreferences = getSharedPreferences("Login", Context.MODE_PRIVATE)
+//                val sharedPrefs = sharedPreferences.edit()
+//                sharedPrefs.putString("userName",email.text.toString())
+//                sharedPrefs.putString("password",password.text.toString())
+//                sharedPrefs.apply()
+//                val intent = Intent(this,LogoutActivity::class.java)
+//                startActivity(intent)
+//            }, 3000)
+
+
     }
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int){
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
     }
 
     override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        val email =email.text.toString()
-        val password =password.text.toString()
+        val email = email.text.toString()
+        val password = password.text.toString()
         loginBtn.isEnabled = email.trim().isNotEmpty() && password.trim().isNotEmpty()
+        signUp.isEnabled = email.trim().isNotEmpty() && password.trim().isNotEmpty()
     }
 
     override fun afterTextChanged(p0: Editable?) {
--- app/src/main/java/com/example/myapplication/fragments/CallsFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/CallsFragment.kt
@@ -1,15 +1,31 @@
 package com.example.myapplication.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentCallsBinding
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_calls.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,10 +37,16 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CallsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CallsFragment : Fragment() {
+class CallsFragment : Fragment(), GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
+     var lat: String? = null
+    var long: String? = null
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var googleApiClient: GoogleApiClient
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +54,10 @@ class CallsFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        buildGoogleApiClient()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+
     }
 
     override fun onCreateView(
@@ -84,5 +110,150 @@ class CallsFragment : Fragment() {
         }
         val alertDialog = alertBuilder.create()
         alertDialog.show()
+    }
+
+    fun save(view: View) {
+        val permissionName = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        val canWrite: Boolean?
+
+        val permission = ActivityCompat.checkSelfPermission(requireActivity(), permissionName)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(permissionName), 100)
+        }
+        canWrite = true
+        if (canWrite){
+            writeStorage()
+        }
+
+    }
+
+    private fun writeStorage() {
+        try {
+            val folder: File? = requireActivity().getExternalFilesDir("folder")
+            val myFile = File(folder, "notes.txt")
+            val editor = FileOutputStream(myFile)
+            editor.write(phoneEditText.text.toString().toByteArray())
+            editor.close()
+            phoneEditText.text.clear()
+            Toast.makeText(requireActivity(), "Saved ", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(requireActivity(), "$e", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun read(view: View) {
+        try {
+            val folder: File? = requireActivity().getExternalFilesDir("folder")
+            val myFile = File(folder, "notes.txt")
+            val editor = FileInputStream(myFile)
+            editor.bufferedReader().forEachLine {
+                val s = StringBuilder()
+                s.append(it)
+                phoneEditText.setText(s)
+            }
+            Toast.makeText(requireActivity(), "Data read ", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(requireActivity(), "$e", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+    fun getMyLocation(view: View) {
+
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissionName = android.Manifest.permission.ACCESS_FINE_LOCATION
+            requestPermissions(arrayOf(permissionName),25)
+            return
+        }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+            lat = it.latitude.toString()
+            long = it.longitude.toString()
+            showLocationAlertDialog(lat!!, long!!)
+        }
+    }
+
+    fun saveToFirebase(view: View){
+       val phone = phoneEditText.text.toString()
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("Message")
+//        myRef.child("Message").setValue(phone)
+//        Toast.makeText(requireActivity(),"done",Toast.LENGTH_LONG).show()
+
+        myRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val  a = snapshot.getValue(String::class.java)
+                phoneEditText.setText(a)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+
+
+    }
+    private  fun showLocationAlertDialog(lat:String,long:String){
+        val alertBuilder = AlertDialog.Builder(activity)
+        alertBuilder.setTitle("Location")
+        alertBuilder.setMessage("Your Location is $lat , $long  ")
+
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()
+    }
+    override fun onStart() {
+        super.onStart()
+        googleApiClient.connect()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.isNotEmpty()){
+            when(requestCode){
+                25 ->
+                    if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                            lat = it.latitude.toString()
+                            long = it.longitude.toString()
+                            showLocationAlertDialog(lat!!, long!!)
+                        }
+
+                    }
+            }
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+    }
+
+   private fun buildGoogleApiClient(){
+        googleApiClient = GoogleApiClient.Builder(requireActivity())
+            .addApi(LocationServices.API).addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .build()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        googleApiClient.disconnect()
     }
 }
